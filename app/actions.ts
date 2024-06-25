582176873a0d9ee56248668878b91db09c1b6847
Michael Haar <michael.haar92@gmail.com>
2024-06-25 11:48:18 +0200

    add zod api input validation

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,16 @@
 "use server";
 
-type GetUserParams = {
-  email: string;
-  password: string;
-};
+import { z } from "zod";
 
-export async function getUser({ email, password }: GetUserParams) {
+const userParamsSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+type GetUserParams = z.infer<typeof userParamsSchema>;
+
+export async function getUser(params: GetUserParams) {
+  const { email, password } = userParamsSchema.parse(params);
+
   // simulate a db call
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return { name: "Michi", email: "michi@asdf.at" };
